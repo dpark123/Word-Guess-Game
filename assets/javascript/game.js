a3575816109930388607dd43f49cf0bd44441802
Donald Park <donaldjpark@gmail.com>
2019-06-04 18:20:56 -0700

    linked css and js files to index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+document.onkeyup = function(event){
+    console.log("you pressed a button");
+    console.log(event.key);//event can be named anything but needs to be the same in function declaration and when called.
+   }
